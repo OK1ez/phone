8062b3f145c02034b0afc3dfac9e4203b193f443
OKiez <87883296+OK1ez@users.noreply.github.com>
2024-09-07 22:53:24 +0200

    Update bleeter-app.tsx

--- src/apps/bleeter/bleeter-app.tsx
+++ src/apps/bleeter/bleeter-app.tsx
@@ -1,12 +1,12 @@
 import { BleeterNav } from "./components/nav";
 import { BleeterHomePage } from "./pages/home/home-page";
-import { BleeterSearchPage } from "./pages/search/search-page";
+// import { BleeterSearchPage } from "./pages/search/search-page";
 
 export function BleeterApp() {
   return (
     <div className="flex flex-col w-full h-full bg-background">
       <BleeterHomePage />
-      <BleeterSearchPage />
+      {/* <BleeterSearchPage /> */}
       <BleeterNav />
     </div>
   );
