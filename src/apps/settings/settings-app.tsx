f3d3c8fd711fccfa8ff611f020927e95d39e0010
OKiez <87883296+OK1ez@users.noreply.github.com>
2024-09-13 21:35:19 +0200

    feat: settings app, descriptions, animations, open side

--- src/apps/settings/settings-app.tsx
+++ src/apps/settings/settings-app.tsx
@@ -1,89 +1,164 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Switch } from "@/components/ui/switch";
-import { AudioLines, BellDot, ChevronRight, Image, PlaneTakeoff, ScanFace, Settings, Smartphone, Video } from "lucide-react";
+import { AudioLines, BellDot, ChevronLeft, ChevronRight, Cloud, Image, PlaneTakeoff, ScanFace, Settings, Smartphone, Video } from "lucide-react";
+import { motion, AnimatePresence } from 'framer-motion';
+import { useEffect, useState } from "react";
 
 export function SettingsApp() {
+  const [selectedConversation, setSelectedConversation] = useState<string | null>(null);
+  const [hasBacked, setHasBacked] = useState(false);
+
+  useEffect(() => {
+    if (selectedConversation) {
+      setHasBacked(true);
+    }
+  }, [selectedConversation]);
+
+  function onBack() {
+    setSelectedConversation(null);
+    setHasBacked(true);
+  }
+
+  const [isAirplaneModeOn, setIsAirplaneModeOn] = useState(false);
+
+  const toggleAirplaneMode = () => {
+    setIsAirplaneModeOn(prev => !prev);
+  };
+
+  const [isStreamerModeOn, setIsStreamerModeOn] = useState(false);
+
+  const toggleStreamerMode = () => {
+    setIsStreamerModeOn(prev => !prev);
+  };
+  
   return (
     <div className="flex flex-col w-full h-full bg-background">
-      <header className="flex flex-col w-full pb-4 border-b">
-        <div className="flex items-center justify-center w-full h-5 gap-4 px-6 mt-16">
-          <p className="font-medium">Settings</p>
-        </div>
-      </header>
-      <ScrollArea className="">
-        <button className="flex items-center justify-between w-full h-20 px-6 border-b hover:bg-secondary/70 dark:hover:bg-secondary/20">
-          <div className="flex items-center space-x-4">
-            <Avatar>
-              <AvatarFallback>
-                O
-              </AvatarFallback>
-              <AvatarImage src="github.com/" />
-            </Avatar>
-            <p>Cloud backup</p>
-          </div>
-          <ChevronRight className="w-4 h-4 text-gray-400" />
-        </button>
-        <button className="flex items-center justify-between w-full px-6 border-b h-14 hover:bg-secondary/70 dark:hover:bg-secondary/20">
-          <div className="flex items-center space-x-4">
-            <PlaneTakeoff />
-            <p>Airplane mode</p>
-          </div>
-          <Switch />
-        </button>
-        <button className="flex items-center justify-between w-full px-6 border-b h-14 hover:bg-secondary/70 dark:hover:bg-secondary/20">
-          <div className="flex items-center space-x-4">
-            <Video />
-            <p>Streamer mode</p>
-          </div>
-          <Switch />
-        </button>
-        <button className="flex items-center justify-between w-full px-6 border-b h-14 hover:bg-secondary/70 dark:hover:bg-secondary/20">
-          <div className="flex items-center space-x-4">
-            <BellDot />
-            <p>Notifications</p>
-          </div>
-          <ChevronRight className="w-4 h-4 text-gray-400" />
-        </button>
-        <button className="flex items-center justify-between w-full px-6 border-b h-14 hover:bg-secondary/70 dark:hover:bg-secondary/20">
-          <div className="flex items-center space-x-4">
-            <AudioLines />
-            <p>Sounds</p>
-          </div>
-          <ChevronRight className="w-4 h-4 text-gray-400" />
-        </button>
-        <button className="flex items-center justify-between w-full px-6 border-b h-14 hover:bg-secondary/70 dark:hover:bg-secondary/20">
-          <div className="flex items-center space-x-4">
-            <Settings />
-            <p>General</p>
-          </div>
-          <ChevronRight className="w-4 h-4 text-gray-400" />
-        </button>
-        <button className="flex items-center justify-between w-full px-6 border-b h-14 hover:bg-secondary/70 dark:hover:bg-secondary/20">
-          <div className="flex items-center space-x-4">
-            <ScanFace />
-            <p>Unlock</p>
-          </div>
-          <ChevronRight className="w-4 h-4 text-gray-400" />
-        </button>
-        <button className="flex items-center justify-between w-full px-6 border-b h-14 hover:bg-secondary/70 dark:hover:bg-secondary/20">
-          <div className="flex items-center space-x-4">
-            <Smartphone />
-            <p>Display and brightness</p>
-          </div>
-          <ChevronRight className="w-4 h-4 text-gray-400" />
-        </button>
-        <button className="flex items-center justify-between w-full px-6 border-b h-14 hover:bg-secondary/70 dark:hover:bg-secondary/20">
-          <div className="flex items-center space-x-4">
-            <Image />
-            <p>Wallpapers</p>
-          </div>
-          <ChevronRight className="w-4 h-4 text-gray-400" />
-        </button>
-      </ScrollArea>
-      {/* <div className="w-full py-4 text-center">
-        <p className="text-gray-400">Version 1.0.0</p>
-      </div> */}
+      <AnimatePresence>
+        {selectedConversation ? (
+          <motion.div
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100%" }}
+            transition={{ type: "spring", stiffness: 300, damping: 30 }}
+            className="absolute z-20 flex flex-col w-full h-full overflow-hidden bg-background"
+          >
+            <header className="flex items-center w-full gap-4 px-6 pb-4 mt-[4.5rem] border-b">
+              <button onClick={onBack}>
+                <ChevronLeft className="w-6 h-6" />
+              </button>
+              <p className="font-medium">{selectedConversation}</p>
+            </header>
+            
+            <ScrollArea className="flex-grow w-full p-6 pb-0 overflow-y-auto">
+              Settings and shit here
+            </ScrollArea>
+          </motion.div>
+        ) : (
+          <motion.div
+            key="recentConversations"
+            initial={hasBacked ? { x: "-100%" } : {}}
+            animate={{ x: 0 }}
+            exit={hasBacked ? { x: "-100%" } : {}}
+            transition={{ type: "spring", stiffness: 300, damping: 30 }}
+          >
+            <header className="flex items-center justify-between w-full gap-4 px-6 pb-4 mt-[4.5rem] border-b">
+              <p className="font-medium">Settings</p>
+            </header>
+            <ScrollArea className="flex flex-col w-full h-full max-h-[57rem] overflow-y-auto">
+              <button onClick={() => setSelectedConversation('cloud')} className="flex items-center justify-between w-full h-20 px-6 border-b hover:bg-secondary/70 dark:hover:bg-secondary/20">
+                <div className="flex items-center space-x-4">
+                  <Cloud />
+                  <div className="text-left">
+                    <p className="text-base">Cloud</p>
+                    <p className="text-sm text-gray-400">Save everything, even your digital junk drawer!</p>
+                  </div>
+                </div>
+                <ChevronRight className="w-4 h-4 text-gray-400" />
+              </button>
+              <button onClick={() => toggleAirplaneMode()} className="flex items-center justify-between w-full h-20 px-6 border-b hover:bg-secondary/70 dark:hover:bg-secondary/20">
+                <div className="flex items-center space-x-4">
+                  <PlaneTakeoff />
+                  <div className="text-left">
+                    <p className="text-base">Airplane mode</p>
+                    <p className="text-sm text-gray-400">Fake a flight and dodge distractions!</p>
+                  </div>
+                </div>
+                <Switch checked={isAirplaneModeOn} onCheckedChange={toggleAirplaneMode} />
+              </button>
+              <button onClick={() => toggleStreamerMode()} className="flex items-center justify-between w-full h-20 px-6 border-b hover:bg-secondary/70 dark:hover:bg-secondary/20">
+                <div className="flex items-center space-x-4">
+                  <Video />
+                  <div className="text-left">
+                    <p className="text-base">Streamer mode</p>
+                    <p className="text-sm text-gray-400">Hides sensitive information</p>
+                  </div>
+                </div>
+                <Switch checked={isStreamerModeOn} onCheckedChange={toggleStreamerMode} />
+              </button>
+              <button onClick={() => setSelectedConversation('notifications')} className="flex items-center justify-between w-full h-20 px-6 border-b hover:bg-secondary/70 dark:hover:bg-secondary/20">
+                <div className="flex items-center space-x-4">
+                  <BellDot />
+                  <div className="text-left">
+                    <p className="text-base">Notifications</p>
+                    <p className="text-sm text-gray-400">Where chaos begins (or ends)</p>
+                  </div>
+                </div>
+                <ChevronRight className="w-4 h-4 text-gray-400" />
+              </button>
+              <button onClick={() => setSelectedConversation('sounds')} className="flex items-center justify-between w-full h-20 px-6 border-b hover:bg-secondary/70 dark:hover:bg-secondary/20">
+                <div className="flex items-center space-x-4">
+                  <AudioLines />
+                  <div className="text-left">
+                    <p className="text-base">Sounds</p>
+                    <p className="text-sm text-gray-400">Change ringtone and alerts</p>
+                  </div>
+                </div>
+                <ChevronRight className="w-4 h-4 text-gray-400" />
+              </button>
+              <button onClick={() => setSelectedConversation('general')} className="flex items-center justify-between w-full h-20 px-6 border-b hover:bg-secondary/70 dark:hover:bg-secondary/20">
+                <div className="flex items-center space-x-4">
+                  <Settings />
+                  <div className="text-left">
+                    <p className="text-base">General</p>
+                    <p className="text-sm text-gray-400">All the basic stuff that actually matters</p>
+                  </div>
+                </div>
+                <ChevronRight className="w-4 h-4 text-gray-400" />
+              </button>
+              <button onClick={() => setSelectedConversation('unlock')} className="flex items-center justify-between w-full h-20 px-6 border-b hover:bg-secondary/70 dark:hover:bg-secondary/20">
+                <div className="flex items-center space-x-4">
+                  <ScanFace />
+                  <div className="text-left">
+                    <p className="text-base">Unlock</p>
+                    <p className="text-sm text-gray-400">Let your phone judge your face</p>
+                  </div>
+                </div>
+                <ChevronRight className="w-4 h-4 text-gray-400" />
+              </button>
+              <button onClick={() => setSelectedConversation('display')} className="flex items-center justify-between w-full h-20 px-6 border-b hover:bg-secondary/70 dark:hover:bg-secondary/20">
+                <div className="flex items-center space-x-4">
+                  <Smartphone />
+                  <div className="text-left">
+                    <p className="text-base">Display and brightness</p>
+                    <p className="text-sm text-gray-400">Adjust until you go blind, or it's perfect</p>
+                  </div>
+                </div>
+                <ChevronRight className="w-4 h-4 text-gray-400" />
+              </button>
+              <button onClick={() => setSelectedConversation('wallpapers')} className="flex items-center justify-between w-full h-20 px-6 border-b hover:bg-secondary/70 dark:hover:bg-secondary/20">
+                <div className="flex items-center space-x-4">
+                  <Image />
+                  <div className="text-left">
+                    <p className="text-base">Wallpapers</p>
+                    <p className="text-sm text-gray-400">Find the image that you'll change in 2 days</p>
+                  </div>
+                </div>
+                <ChevronRight className="w-4 h-4 text-gray-400" />
+              </button>
+            </ScrollArea>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
