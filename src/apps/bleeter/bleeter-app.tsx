f106cbe924c2078494fe9d7ebfbe8ec7a9f4cec9
OKiez <gkfjdjswifjs234567@gmail.com>
2024-09-15 23:32:08 +0200

    chore(bleeter): new post button on all pages

--- src/apps/bleeter/bleeter-app.tsx
+++ src/apps/bleeter/bleeter-app.tsx
@@ -4,6 +4,7 @@ import { BleeterHomePage } from "./pages/home/home-page";
 import { BleeterSearchPage } from "./pages/search/search-page";
 import { BleeterNotificationsPage } from './pages/notifications/notifications-page';
 import { BleeterProfilePage } from './pages/profile/profile-page';
+import { Plus } from 'lucide-react';
 
 export function BleeterApp() {
   const [activePage, setActivePage] = useState('home');
@@ -26,6 +27,9 @@ export function BleeterApp() {
   return (
     <div className="flex flex-col w-full h-full bg-background">
       {renderPage()}
+      <button className="absolute bottom-0 right-0 flex items-center justify-center m-4 rounded-full shadow-md w-14 h-14 mb-28 bg-secondary hover:bg-muted group">
+        <Plus className="text-gray-400 group-hover:text-foreground" />
+      </button>
       <BleeterNav activePage={activePage} setActivePage={setActivePage} />
     </div>
   );
