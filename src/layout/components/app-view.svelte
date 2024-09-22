6b39cec1fbe44eaaf78f3986080490382c5fa93d
OKiez <gkfjdjswifjs234567@gmail.com>
2024-09-22 18:58:41 +0200

    chore: lazy load apps

--- src/layout/components/app-view.svelte
+++ src/layout/components/app-view.svelte
@@ -2,29 +2,42 @@
   import { SELECTED_APP } from "@/stores/phone";
   import { scale } from 'svelte/transition';
   import Indicator from "./indicator.svelte";
-  import BleeterApp from "@/apps/bleeter/bleeter-app.svelte";
-  import SettingsApp from "@/apps/settings/settings-app.svelte";
-  import MessagesApp from "@/apps/messages/messages-app.svelte";
-  import PhoneApp from "@/apps/phone/phone-app.svelte";
+  import { onMount } from 'svelte';
+
+  let AppComponent: any = null;
+
+  $: loadComponent($SELECTED_APP);
+
+  async function loadComponent(appName: string | null) {
+    if (!appName) {
+      AppComponent = null;
+      return;
+    }
+
+    try {
+      const module = await import(`@/apps/${appName}/${appName}-app.svelte`);
+      AppComponent = module.default;
+    } catch (error) {
+      console.error('Failed to load component:', error);
+      AppComponent = null;
+    }
+  }
 
   function closeApp() {
     SELECTED_APP.set(null);
   }
 
-  const appComponents: { [key: string]: any } = {
-    bleeter: BleeterApp,
-    settings: SettingsApp,
-    messages: MessagesApp,
-    phone: PhoneApp,
-  };
+  onMount(() => {
+    loadComponent($SELECTED_APP);
+  });
 </script>
 
 <div
   class="relative flex flex-col w-full h-full bg-background"
-  transition:scale={{ start: 0.5, duration: 250 }} 
+  transition:scale={{ start: 0.5, duration: 250 }}
 >
-  {#if $SELECTED_APP}
-   <svelte:component this={appComponents[$SELECTED_APP]} />
+  {#if $SELECTED_APP && AppComponent}
+    <svelte:component this={AppComponent} />
   {/if}
   <Indicator on:click={closeApp} />
 </div>
