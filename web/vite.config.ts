f8179853a8be3aeef006dd7ccb7be79e893de4f2
OKiez <gkfjdjswifjs234567@gmail.com>
2024-09-28 14:21:30 +0200

    feat: rollup visualizer

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), visualizer() as PluginOption],
+  base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    emptyOutDir: true,
+    outDir: './build',
+    assetsDir: './',
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
 })
