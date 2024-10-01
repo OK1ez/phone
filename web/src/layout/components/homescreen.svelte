8945ee333c6bee92f10830dccef5566a9679a9ca
OKiez <gkfjdjswifjs234567@gmail.com>
2024-10-01 21:17:42 +0200

    Update homescreen.svelte

--- web/src/layout/components/homescreen.svelte
+++ web/src/layout/components/homescreen.svelte
@@ -11,10 +11,18 @@
   class="absolute flex justify-center w-full h-full"
   style="background-image: url({Background});"
 >
-  <div class="grid grid-cols-4 gap-8 absolute mt-16">
+  <div class="grid grid-cols-4 gap-8 absolute mt-20">
     {#each Object.entries($APPS) as [appName, app]}
-      <button on:click={() => openApp(appName)}>
-        <span class="text-sm">{app.name}</span>
+      <button
+        class="flex flex-col items-center justify-center rounded-lg space-y-1"
+        on:click={() => openApp(appName)}
+      >
+        <div
+          src={app.icon}
+          alt={appName}
+          class="size-[4.5rem] rounded-2xl bg-secondary"
+        ></div>
+        <p class="text-center text-sm">{app.name}</p>
       </button>
     {/each}
   </div>
