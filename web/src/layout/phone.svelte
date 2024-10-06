0e2e3885cb947f6562a5f215c2b9ab9b1d2be2ca
OKiez <gkfjdjswifjs234567@gmail.com>
2024-10-06 16:47:01 +0200

    chore: hide homescreen if selected app

--- web/src/layout/phone.svelte
+++ web/src/layout/phone.svelte
@@ -1,16 +1,33 @@
 <script>
+  import { onMount } from "svelte";
   import { IS_LOCKED, SELECTED_APP } from "@/stores/phone";
   import Lockscreen from "./components/lockscreen.svelte";
   import AppView from "./components/app-view.svelte";
   import Homescreen from "./components/homescreen.svelte";
   import PhoneWrapper from "./components/phone-wrapper.svelte";
+
+  let showHomescreen = true;
+
+  $: {
+    if ($SELECTED_APP) {
+      setTimeout(() => {
+        showHomescreen = false;
+      }, 250);
+    } else {
+      showHomescreen = true;
+    }
+  }
 </script>
 
 <PhoneWrapper>
-  <Homescreen />
   {#if $IS_LOCKED}
     <Lockscreen />
-  {:else if $SELECTED_APP}
-    <AppView />
+  {:else}
+    {#if showHomescreen}
+      <Homescreen />
+    {/if}
+    {#if $SELECTED_APP}
+      <AppView />
+    {/if}
   {/if}
 </PhoneWrapper>
